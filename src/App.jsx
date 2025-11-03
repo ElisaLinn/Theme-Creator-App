fdfcbf99a4532cbfe9c9373d0dc7f29fd9aaa03e
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-11-03 14:03:43 +0100

    you can add new cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "./Components/Color/ColorForm";
+import { useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleSubmitColor(colorData) {
+    const newColor = {
+      id: crypto.randomUUID(),
+      ...colorData
+    };
+    setColors([...colors, newColor]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
+      <ColorForm onSubmitColor={handleSubmitColor} />
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
